48500b2d4660303c109721b8688e8f5c95bb05ad
Sergey <sergey.morenets@yandex.ua>
2019-04-20 16:54:51 +0300

    Task 10.1 Added logging interceptor

---
+++ rest-task5/src/main/java/it/discovery/interceptor/LoggingInterceptor.java
@@ -0,0 +1,22 @@
+package it.discovery.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoggingInterceptor extends HandlerInterceptorAdapter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler)
+            throws Exception {
+        HandlerMethod method = (HandlerMethod) handler;
+        LOGGER.info("REST-service {} called", method.getMethod().getName());
+        return true;
+    }
+}
