b42bc6586ff9db038c9c237bf4261f09f0c60a1f
Sergey <sergey.morenets@yandex.ua>
2019-04-21 15:06:28 +0300

    Task 15. Added parametrized test

--- rest-task15/src/test/java/it/discovery/controller/BookControllerTest.java
+++ rest-task15/src/test/java/it/discovery/controller/BookControllerTest.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import it.discovery.RestApplication;
 import it.discovery.model.Book;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
@@ -12,6 +15,7 @@ import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -22,6 +26,24 @@ public class BookControllerTest {
     private MockMvc mockMvc;
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    @Nested
+    class GetTests {
+
+    }
+
+    @Nested
+    class PostTests {
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "10000"})
+    void findBook_invalidId_ReturnsNotFound(String id) throws Exception {
+        ResultActions actions = mockMvc.perform(get("/book/" + id));
+
+        actions.andExpect(status().isNotFound());
+    }
 
     @Test
     void saveBook_emptyStorage_bookSaved() throws Exception {
