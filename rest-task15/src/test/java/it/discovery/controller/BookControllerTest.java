3a4c7a6d5bbe4a215a8a3aedc20400ad79f6bd7e
Sergey <sergey.morenets@yandex.ua>
2019-04-21 14:49:28 +0300

    Task 15. Added integration test for POST method

--- rest-task15/src/test/java/it/discovery/controller/BookControllerTest.java
+++ rest-task15/src/test/java/it/discovery/controller/BookControllerTest.java
@@ -1,27 +1,42 @@
 package it.discovery.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.discovery.RestApplication;
+import it.discovery.model.Book;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import it.discovery.RestApplication;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringJUnitWebConfig(RestApplication.class)
 @AutoConfigureMockMvc
 public class BookControllerTest {
-	@Autowired
+    @Autowired
     private MockMvc mockMvc;
-    
+
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
     @Test
-    public void test() { 
-    	assertTrue(true);
+    void saveBook_emptyStorage_bookSaved() throws Exception {
+        Book book = new Book();
+        book.setAuthor("Gosling");
+        book.setName("Spring MVC");
+        book.setYear(2020);
+
+        ResultActions actions = mockMvc.perform(post("/book")
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .content(MAPPER.writeValueAsString(book)));
+
+        actions.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.author", Matchers.equalTo(book.getAuthor())));
     }
 
 }
